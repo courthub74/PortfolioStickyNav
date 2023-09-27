03464380de907a4159000385cac7220cce9af414
Courdevelops <courdevelops@gmail.com>
2023-09-27 17:31:15 -0400

    blank sportfolio slide in canvases positioned

---
+++ skillsbuttons.js
@@ -0,0 +1,15 @@
+// Skills Buttons
+
+// query EACH button
+
+// HTML
+let html_skill = document.getElementById('html_skill');
+
+// add event listener to each button
+
+// in the event listener:
+    // make the icon dissapear
+    // make the title appear
+    // after 5 seconds:
+        // reverse everything
+
